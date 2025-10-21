3efc1edc3347d798d1d6b24a96f47d672875ec7d
phucnguyen66 <95067768+phucnguyen66@users.noreply.github.com>
2025-10-22 02:47:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.use("/upload", uploadRouter);
 app.use("/point", pointRouter);
 app.use("/mail", mailRouter);
 
-// --- Chạy 1 port duy nhất ---
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Main backend running at http://localhost:${PORT}`));
+module.exports = router;
+
+
