1b4823084c39d3faa666b35c8163ee6430ecbc34
phucnguyen66 <95067768+phucnguyen66@users.noreply.github.com>
2025-10-22 16:22:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
-require('dotenv').config({ path: '../.env' }); // ../ trỏ lên một cấp
+require('dotenv').config({ path: './.env' }); // ../ trỏ lên một cấp
 const cloudinary = require('cloudinary').v2;
 const express = require("express");
 const router = express.Router(); // thêm dòng này ✅
